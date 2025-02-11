a64ee6b1f608d7ce7bbdaf04b1ebd3dd42f3c5aa
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-11 12:03:46 +0530

    ss

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-const parent = React.createElement(
-  "div",
-  { id: "child" },
-  React.createElement("h1", {}, "i am a h1 tag")
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am a h1 tag"),
+    React.createElement("h2", {}, "i am a h1 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am a h1 tag"),
+    React.createElement("h2", {}, "i am a h1 tag"),
+  ]),
+]);
 
 const heading = React.createElement(
   "h1",
