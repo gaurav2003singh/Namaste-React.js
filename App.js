ec94555756014e13834e599b433d9e47f28704bc
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-11 11:40:24 +0530

    nested html by react

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
-const heading = React.createElement("h1", {id: "heading"}, "hello from react");
+const parent = React.createElement(
+  "div",
+  { id: "child" },
+  React.createElement("h1", {}, "i am a h1 tag")
+);
 
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello from react"
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
