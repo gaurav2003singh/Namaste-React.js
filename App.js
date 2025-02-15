771fac354aa5f6a97d92c7126a33331b5096e2a0
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-15 13:14:36 +0530

    ignite_our_app

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
+import React from "react" ;
+import ReactDOM from "react-dom/client";
+
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am a h1 tag"),
+    React.createElement("h1", {}, "this is namaste react"),
     React.createElement("h2", {}, "i am a h1 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 I am learning React.js from beginning again.
 i have already learnd react but now i am diving deep into it.
 
+# parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement 
+- File Watching Algorithm - written in C++
+- Caching - faster Bhuilds
+- image optimization
+- minification
+- bundling
+- compressing
+- 
+
