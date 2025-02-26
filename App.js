4748dfad4ac07474355a1dd48035f874944bbaaf
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-26 21:22:45 +0530

    code

--- App.js
+++ App.js
@@ -1,24 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement ==> Object => HTMLelement(render)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="C:\Users\ajayc\OneDrive\Desktop\bird-colorful-logo-gradient-vector_343694-1365.avif"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//React element
+const RastaurantCard = (props) => {
+  const {resName,cuisine} = props; 
+  return (
+    <div className="res-card">
+      <img
+        src="https://tse1.mm.bing.net/th?id=OIP.dAwpD5xrPNSDV10bs8ulqQHaEK&pid=Api&P=0&h=220"
+        alt=""
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <div className="info">
+        <h4>4.4</h4>
+        <h4>38 minutes</h4>
+      </div>
+    </div>
+  );
+};
 
-const Title =  (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input type="text" placeholder="search here" />
+      </div>
+      <div className="res-container">
+        <RastaurantCard
+          resName="Meghana foods"
+          cuisine="Biryani,North Indian,Asian"
+        />
+        <RastaurantCard resName="KFC" cuisine="Burger,Fast Food" />
+        <RastaurantCard
+          resName="Meghana foods"
+          cuisine="Biryani,North Indian,Asian"
+        />
+        <RastaurantCard
+          resName="Meghana foods"
+          cuisine="Biryani,North Indian,Asian"
+        />
+      </div>
+    </div>
+  );
+};
 
-//React functional Components
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    {Title }
-    <h1 className="heading">Namaste React functional components</h1>
-  </div>
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
