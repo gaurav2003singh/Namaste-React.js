460e2deeb1305293f49bd3eda98784a14b111ffe
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-03-04 15:43:23 +0530

    hookscopleted

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,80 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
-// import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
   //Local state Variable - Super powerful variable.
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      type: "restaurant",
-      data: {
-        type: "F",
-        id: "121603",
-        name: "Kannur Food Point",
-        uuid: "51983905-e698-4e31-b0d7-e376eca56320",
-        city: "1",
-        area: "Tavarekere",
-        totalRatingsString: "10000+ ratings",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        tags: [],
-        costForTwo: 30000,
-        costForTwoString: "₹300 FOR TWO",
-        deliveryTime: 24,
-        minDeliveryTime: 24,
-        maxDeliveryTime: 24,
-        slaString: "24 MINS",
-        lastMileTravel: 3,
-        avgRating: "3.9",
-      },
-    },
-    {
-      type: "restaurant",
-      data: {
-        type: "F",
-        id: "121602",
-        name: "bhatt bhojnalay",
-        uuid: "51983905-e698-4e31-b0d7-e376eca56320",
-        city: "1",
-        area: "Tavarekere",
-        totalRatingsString: "10000+ ratings",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        tags: [],
-        costForTwo: 30000,
-        costForTwoString: "₹300 FOR TWO",
-        deliveryTime: 24,
-        minDeliveryTime: 24,
-        maxDeliveryTime: 24,
-        slaString: "24 MINS",
-        lastMileTravel: 3,
-        avgRating: "4.5",
-      },
-    },
-    {
-      type: "restaurant",
-      data: {
-        type: "F",
-        id: "121608",
-        name: "rawat bhojnalay",
-        uuid: "51983905-e698-4e31-b0d7-e376eca56320",
-        city: "1",
-        area: "Tavarekere",
-        totalRatingsString: "10000+ ratings",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        tags: [],
-        costForTwo: 30000,
-        costForTwoString: "₹300 FOR TWO",
-        deliveryTime: 24,
-        minDeliveryTime: 24,
-        maxDeliveryTime: 24,
-        slaString: "24 MINS",
-        lastMileTravel: 3,
-        avgRating: "4.1",
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   //normal js variable.
   let listOfRestaurantsjs = [
